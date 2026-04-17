cfd3634980efeb8a212d4ffeff7b026721b84e57
ToruSmith <x83991@gmail.com>
2026-04-17 17:58:58 +0800

    Update model.py

--- backend/model.py
+++ backend/model.py
@@ -1,19 +1,19 @@
-import tensorflow as tf
-from tensorflow.keras import layers
+def build_model(input_shape, filters=32, lr=0.001):
+    import tensorflow as tf
+    from tensorflow.keras import layers
 
-def build_model(filters=32, lr=0.001):
     model = tf.keras.Sequential([
-        layers.Conv2D(filters, (2,2), activation='relu', input_shape=(5,30,1)),
-        layers.MaxPooling2D((1,2)),
+        layers.Conv1D(filters, 3, activation='relu', input_shape=input_shape),
+        layers.MaxPooling1D(2),
 
         layers.Flatten(),
         layers.Dense(32, activation='relu'),
-        layers.Dense(3, activation='softmax')
+        layers.Dense(1, activation='sigmoid')
     ])
 
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=lr),
-        loss='categorical_crossentropy',
+        optimizer=tf.keras.optimizers.Adam(lr),
+        loss='binary_crossentropy',
         metrics=['accuracy']
     )
 
