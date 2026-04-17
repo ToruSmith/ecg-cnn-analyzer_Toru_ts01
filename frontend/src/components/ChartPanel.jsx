e4fc355c8d8c8c52cd02726b0927e0183a907fc0
ToruSmith <x83991@gmail.com>
2026-04-17 18:04:06 +0800

    Update ChartPanel.jsx

--- frontend/src/components/ChartPanel.jsx
+++ frontend/src/components/ChartPanel.jsx
@@ -1,1 +1,16 @@
-<pre>{JSON.stringify(data)}</pre>
+import EquityChart from "./EquityChart";
+import LossChart from "./LossChart";
+
+export default function ChartPanel({ data }) {
+  if (!data) return null;
+
+  return (
+    <div>
+      <h3>📈 Equity Curve</h3>
+      <EquityChart equity={data.equity_curve} />
+
+      <h3>📉 Training Loss</h3>
+      <LossChart history={data.history} />
+    </div>
+  );
+}
