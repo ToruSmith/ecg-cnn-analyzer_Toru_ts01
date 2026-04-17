3c7dd2f99a272cd20bb137031cecd1e01ff32773
ToruSmith <x83991@gmail.com>
2026-04-17 18:03:50 +0800

    Update ChartPanel.jsx

--- frontend/src/components/ChartPanel.jsx
+++ frontend/src/components/ChartPanel.jsx
@@ -1,13 +1,1 @@
-export default function ChartPanel({ data }) {
-  if (!data) return null;
-
-  return (
-    <div>
-      <h3>Loss</h3>
-      <pre>{JSON.stringify(data.history, null, 2)}</pre>
-
-      <h3>Equity Curve</h3>
-      <pre>{JSON.stringify(data.equity_curve)}</pre>
-    </div>
-  );
-}
+<pre>{JSON.stringify(data)}</pre>
